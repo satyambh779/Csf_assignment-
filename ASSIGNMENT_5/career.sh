79eeeff4ab2a6e0172bc40ee3fd6f769928f647a
satyambh779 <satyambh779@gmail.com>
2025-11-28 14:19:51 +0530

    Add files via upload

---
+++ ASSIGNMENT_5/career.sh
@@ -0,0 +1,9 @@
+echo "craeting career files"
+
+mkdir -p creer2.0/certification
+mkdir -p career2.0/project
+mkdir -p career2.0/resumes
+mkdir -p career2.0/documentation
+echo "folder created succesfully"
+echo "sytem uptime"
+uptime
